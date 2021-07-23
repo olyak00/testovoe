5b99b4412324c981858aa00b1c71d825e23a2082
Olha_Korniienko <olgakorniienko658@gmail.com>
2021-07-23 16:12:40 +0200

    add

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+
+$url = 'localhost/index.php?function=functionName';
+//$url = ' localhost/store/product?date=2020-02-20';
+//$url = ' localhost/store/product/create?id=5';
+//$url = ' localhost/qweqwe/qwerqwetgwsgh';
+
+$array = parse_url($url);
+
+if ($array[query] == 'function=functionName' ) {
+  functionSome1();
+}
+ elseif ($array[query] == 'date=2020-02-20') {
+    functionSome2();
+ }
+ elseif ($array[query] == 'id=5') {
+    functionSome3();
+} else {
+    error();
+}
+
+function functionSome1()
+{
+    echo 'localhost/index.php?function=functionName';
+}
+
+function functionSome2()
+{
+    echo 'localhost/store/product?date=2020-02-20';
+}
+
+function functionSome3()
+{
+ echo 'localhost/store/product/create?id=5';
+}
+
+function error()
+{
+    echo 'error 404';
+}
+
+?>

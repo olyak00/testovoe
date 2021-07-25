fd9eafd8e6e32a3d320b705112f09830f2e7b47b
Olha_Korniienko <olgakorniienko658@gmail.com>
2021-07-25 12:09:04 +0200

    ingex and php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+ <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Testovoe</title>
+    </head>
+    <body>
+        
 <?php
 
 var_dump($_GET);
@@ -47,3 +56,6 @@ function error()
 }
 */
 ?>
+
+    </body>
+    </html>   
